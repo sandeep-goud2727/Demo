d51854ad6766b92b0ac8b2043853488a91dfd516
sandeep-goud2727 <kumarparamata2727@gmail.com>
2025-10-15 10:07:25 +0530

    Add files via upload

---
+++ VirtualBaseClass.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class A{
+	public:
+		int x=100;
+};
+class B: virtual public A{
+};
+class C: virtual public A{
+};
+class D: public B, public C{
+};
+int main()
+{
+	D obj;
+	cout << obj.x <<endl;
+	return 0;
+}
